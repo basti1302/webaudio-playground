daf2f4c5c91c971154cc80191c8e2dc9921b831a
Bastian Krol <bastian.krol@web.de>
2014-09-18 16:09:12 +0200

    Revert "break it up"
    This reverts commit 75849409301ff5668553bcc42cae02f522d4e0bf.

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-'use strict';
-
-var bpm = 120;
-var beats = 4;
-var initialDelay = .1;
-var startTimeOffset;
-
 var twelvthRootOfTwo = 1.059463094;
 
 var minOctave = 0;
 var maxOctave = 10;
+
+function calculateFrequency(n) {
+  // using equal temperament
+  return 440 * Math.pow(twelvthRootOfTwo, (n - 49));
+}
+
+var toneNames = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'];
+
+// precalculate frequencies
+var octaves = [];
+for (var o = minOctave; o < maxOctave; o++) {
+  var octave = {};
+  octaves[o] = octave;
+  for (var t = 0; t < 12; t++) {
+    var n = o * 12 + t - 8 ;
+    octave[toneNames[t]] = calculateFrequency(n);
+  }
+}
+
+var bpm = 120;
+var beats = 4;
+var initialDelay = .1;
+var start;
 
 function calculateTime(bar, beat, eigth, sixteenth, thirtytwoth) {
   if (typeof bar === 'undefined') { bar = 1 };
@@ -16,45 +32,11 @@ function calculateTime(bar, beat, eigth, sixteenth, thirtytwoth) {
   if (typeof eigth === 'undefined') { eigth = 1 };
   if (typeof sixteenth === 'undefined') { sixteenth = 1 };
   if (typeof thirtytwoth === 'undefined') { thirtytwoth = 1 };
-  return startTimeOffset + ((bar-1) * beats + (beat-1) + ((eigth-1) / 2) + ((sixteenth-1) / 4) + ((thirtytwoth-1) / 8)) * 60 / bpm;
+  return start + ((bar-1) * beats + (beat-1) + ((eigth-1) / 2) + ((sixteenth-1) / 4) + ((thirtytwoth-1) / 8)) * 60 / bpm;
 }
 var t = calculateTime;
 
-function play(tune) {
-  precalculateFrequencies();
-  startTimeOffset = audioCtx.currentTime + initialDelay;
-  osc.start(t());
-  for (var i = 0; i < tune.length; i++) {
-    var cmd = tune[i];
-    if (cmd[1] === '-') {
-      osc.stop(t.apply(null, cmd[2]));
-    } else {
-      osc.frequency.setValueAtTime(octaves[cmd[0]][cmd[1]], t.apply(null, cmd[2]));
-    }
-  }
-};
-
-function calculateFrequency(n) {
-  // using equal temperament
-  return 440 * Math.pow(twelvthRootOfTwo, (n - 49));
-}
-
-var octaves = [];
-
-// precalculate frequencies
-function precalculateFrequencies() {
-  var toneNames = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b'];
-  for (var o = minOctave; o < maxOctave; o++) {
-    var octave = {};
-    octaves[o] = octave;
-    for (var t = 0; t < 12; t++) {
-      var n = o * 12 + t - 8 ;
-      octave[toneNames[t]] = calculateFrequency(n);
-    }
-  }
-}
-
-rar audioCtx;
+var audioCtx;
 if (typeof AudioContext !== "undefined") {
     audioCtx = new AudioContext();
 } else if (typeof webkitAudioContext !== "undefined") {
@@ -122,6 +104,19 @@ osc.connect(volumeNode);
 volumeNode.connect(lowPassFilter);
 lowPassFilter.connect(scriptNode);
 scriptNode.connect(audioCtx.destination);
+
+function play(tune) {
+  start = audioCtx.currentTime + initialDelay;
+  osc.start(t());
+  for (var i = 0; i < tune.length; i++) {
+    var cmd = tune[i];
+    if (cmd[1] === '-') {
+      osc.stop(t.apply(null, cmd[2]));
+    } else {
+      osc.frequency.setValueAtTime(octaves[cmd[0]][cmd[1]], t.apply(null, cmd[2]));
+    }
+  }
+};
 
 var tune = [
   [4, 'c', [1, 1]],
